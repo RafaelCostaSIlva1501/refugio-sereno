c1dfee95ca10bc589147da89292ac9d6debe3298
Rafael da Costa Silva <rafael1501.oficial@gmail.com>
2024-06-19 13:17:09 -0300

    Teste para viewport mobile

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Altura da viewport multiplicada por 1% para obter um valor para vh
+let vh = window.innerHeight * 0.01;
+
+// Configura o valor em --vh na raiz do documento
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// Adiciona um listener para atualizar o valor de --vh quando a janela é redimensionada
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
