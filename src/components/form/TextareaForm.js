c9e474fd14ed1795f36715a8ed29cba8ac9e2740
Rafael da Costa Silva <rafael1501.oficial@gmail.com>
2024-06-24 13:57:58 -0300

    Teste com tamanho do textarea

--- src/components/form/TextareaForm.js
+++ src/components/form/TextareaForm.js
@@ -4,7 +4,7 @@ const TextareaForm = (props) => {
             <label htmlFor={props.id}>{props.text}</label>
             <textarea
                 id={props.id}
-                rows={3}
+                rows={4}
                 className="p-1 bg-white-100 rounded text-white-300 outline-none resize-none scrollbar"
             >{props.characterText}</textarea>
         </div>
